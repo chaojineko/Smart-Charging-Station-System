36e6b6d42f3c4b70a928a3b0726c0e8e71796322
超级猫 <15606313+chaojineko@user.noreply.gitee.com>
2025-07-22 16:29:48 +0800

    first commit

---
+++ main.c
@@ -0,0 +1,71 @@
+#include "lvgl/lvgl.h"
+#include "lvgl/demos/lv_demos.h"
+#include <unistd.h>
+#include <pthread.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// 添加UI头文件
+#include "ui/ui.h"
+
+#ifndef PATH_PREFIX
+#define PATH_PREFIX "./"
+#endif
+
+static const char * getenv_default(const char * name, const char * dflt)
+{
+    return getenv(name) ?: dflt;
+}
+
+#if LV_USE_LINUX_FBDEV
+static void lv_linux_disp_init(void)
+{
+    const char * device = getenv_default("LV_LINUX_FBDEV_DEVICE", "/dev/fb0");
+    lv_display_t * disp = lv_linux_fbdev_create();
+
+    lv_linux_fbdev_set_file(disp, device);
+}
+#elif LV_USE_SDL
+static void lv_linux_disp_init(void)
+{
+    const int width  = atoi(getenv("LV_SDL_VIDEO_WIDTH") ?: "800");
+    const int height = atoi(getenv("LV_SDL_VIDEO_HEIGHT") ?: "480");
+
+    lv_sdl_window_create(width, height);
+}
+#else
+#error Unsupported configuration
+#endif
+
+int main(void)
+{
+    lv_init();
+    /*Linux display device init*/
+    lv_linux_disp_init();
+
+#if LV_USE_SDL
+    // init input device
+    lv_sdl_mouse_create();
+    lv_sdl_keyboard_create();
+    lv_sdl_mousewheel_create();
+#endif
+
+#if LV_USE_LINUX_FBDEV
+    // 创建输入设备
+    lv_indev_t * touch = lv_evdev_create(LV_INDEV_TYPE_POINTER, "/dev/input/event0");
+    // 校准输入设备屏幕坐标
+    lv_evdev_set_calibration(touch, 0, 0, 1024, 600); // 黑色边框的屏幕
+    // lv_evdev_set_calibration(touch, 0, 0, 800, 480);  // 蓝色边框的屏幕
+#endif
+
+    // 初始化UI界面
+    ui_init();
+
+    /*Handle LVGL tasks*/
+    while(1) {
+        lv_timer_handler();
+        usleep(5000);
+    }
+    return 0;
+}
